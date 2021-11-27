5276328bd3f9ded531f1ae47b909f64654dd35ef
Nikhil Singh <nikhilbangalor@gmail.com>
2021-11-27 15:27:33 +0530

    orders styling

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./CheckoutProduct.css";
 import { useStateValue } from "./StateProvider";
 
-function CheckoutProduct({ id, image, title, price, rating }) {
+function CheckoutProduct({ id, image, title, price, rating, hideButton }) {
   const [{ basket }, dispatch] = useStateValue();
 
   const removeFromBasket = () => {
@@ -32,8 +32,11 @@ function CheckoutProduct({ id, image, title, price, rating }) {
             .map((_, i) => (
               <p>⭐</p>
             ))}
+          {!hideButton && (
+            <button onClick={removeFromBasket}>Remove from Basket</button>
+          )}
         </div>
-        <button onClick={removeFromBasket}>Remove from basket</button>
+        {/* <button onClick={removeFromBasket}>Remove from basket</button> */}
       </div>
     </div>
   );
